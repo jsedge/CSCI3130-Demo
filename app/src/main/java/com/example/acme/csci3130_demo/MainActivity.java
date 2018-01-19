31fe372d4ffb5d53538eb915dab9a3e0a6f1c1a0
jsedge <KingGuppie@gmail.com>
2018-01-19 15:48:36 -0400

    added button and input box

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+   public void makeText(View view){
+       EditText textbox = findViewById(R.id.editText);
+       String input = textbox.getText().toString();
+
+       TextView display = findViewById(R.id.displayText);
+
+       display.setText(input);
+
     }
 }
